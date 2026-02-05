b95e24ec7e151e3fe365ab86a88c1c041b8389ab
Hyzinx <hyzin633@gmail.com>
2026-02-05 11:44:03 +0200

    Add SandBox Branch to test new widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,21 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp());
+  runApp(MaterialApp(home: SandBox()));
+}
+
+class SandBox extends StatelessWidget {
+  const SandBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("SandBox"),
+        backgroundColor: Colors.grey,
+        centerTitle: true,
+      ),
+      body: Text("SandBox"),
+    );
+  }
 }
