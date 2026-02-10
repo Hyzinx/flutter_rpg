de9b6ca3a9737cfeff96cd077882ed5698eb2cdd
Hyzinx <hyzin633@gmail.com>
2026-02-10 11:08:14 +0200

    Add Enhance Enum why to add conests value as vocation in my example the vocation not have only name but it have title description imag weapon ability the only enum provide to me only one thing  but enhance enum provide me full object has constractor and methods and atributes but we can't make a constractor public but we can make private to make sure no one make object from this enum becase we don't wont any one make a object frrom enum

--- lib/models/character.dart
+++ lib/models/character.dart
@@ -1,11 +1,13 @@
 import 'package:flutter_rpg/models/status.dart';
+import 'package:flutter_rpg/models/vocation.dart';
 
 class Character with Status {
-  Character({required this.id, required this.name, required this.slogan});
+  Character({required this.id, required this.name, required this.slogan,required this.vocation});
 
   final String name;
   final String slogan;
   final String id;
+  final Vocation vocation;
 
   bool get isFav => _isFav;
 
