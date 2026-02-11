6510490e4dff44a1762e08af4982ce8566c78865
Hyzinx <hyzin633@gmail.com>
2026-02-11 14:12:50 +0200

    Add Create Screen and switch to create Screen in main.dart

---
+++ lib/screens/create/create.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Create extends StatefulWidget {
+  const Create({super.key});
+
+  @override
+  State<Create> createState() => _CreateState();
+}
+
+class _CreateState extends State<Create> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Text("Create Screen"),
+    );
+  }
+}
